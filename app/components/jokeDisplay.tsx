83eb08c730f8622e194308440b7d99426c04b2c7
Rúben Carreira <r.carreira@kigroup.de>
2022-01-05 10:40:59 +0000

    refactor: joke component

---
+++ app/components/jokeDisplay.tsx
@@ -0,0 +1,26 @@
+import { Link, Form } from 'remix';
+import type { Joke } from '@prisma/client';
+
+type Props = {
+  joke: Pick<Joke, 'content' | 'name'>;
+  isOwner: boolean;
+  canDelete?: boolean;
+};
+
+export function JokeDisplay({ joke, isOwner, canDelete = true }: Props) {
+  return (
+    <div>
+      <p>Here's your hilarious joke:</p>
+      <p>{joke.content}</p>
+      <Link to=".">{joke.name} Permalink</Link>
+      {isOwner ? (
+        <Form method="post">
+          <input type="hidden" name="_method" value="delete" />
+          <button type="submit" className="button" disabled={!canDelete}>
+            Delete
+          </button>
+        </Form>
+      ) : null}
+    </div>
+  );
+}
--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -3,6 +3,7 @@ import { Link, useLoaderData } from 'remix';
 import type { Joke } from '@prisma/client';
 import { db } from '~/utils/db.server';
 import { getUserId, requireUserId } from '~/utils/session.server';
+import { JokeDisplay } from '~/components/jokeDisplay';
 
 type LoaderData = { joke: Joke; isOwner: boolean };
 
@@ -48,21 +49,7 @@ export const action: ActionFunction = async ({ request, params }) => {
 export default function JokeRoute() {
   const data = useLoaderData<LoaderData>();
 
-  return (
-    <div>
-      <p>Here's your hilarious joke:</p>
-      <p>{data.joke.content}</p>
-      <Link to=".">{data.joke.name} Permalink</Link>
-      {data.isOwner ? (
-        <Form method="post">
-          <input type="hidden" name="_method" value="delete" />
-          <button type="submit" className="button">
-            Delete
-          </button>
-        </Form>
-      ) : null}
-    </div>
-  );
+  return <JokeDisplay joke={data.joke} isOwner={data.isOwner} />;
 }
 
 export function CatchBoundary() {
